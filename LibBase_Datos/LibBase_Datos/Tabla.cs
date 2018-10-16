36a0f0261ce1d06703faaf1dd50b02f643673207
Eliott Ivan Salazar Quiajda <eliottsalazar@gmail.com>
2018-10-15 20:18:32 -0700

    .

--- LibBase_Datos/LibBase_Datos/Tabla.cs
+++ LibBase_Datos/LibBase_Datos/Tabla.cs
@@ -14,7 +14,7 @@ namespace LibBase_Datos
 
         public class Columna
         {
-            public Columna(string nom, TipoDeCampo tipo_columna, int largo_campo, bool es_null, object valor_default, Indice llave, string coment_column)
+            public Columna(string nom, TipoDeCampo tipo_columna, int largo_campo, bool es_null, object valor_default, Indice llave, string coment_column, object value)
             {
                 nom = nombre;
                 tipo_columna = column_type;
@@ -23,6 +23,7 @@ namespace LibBase_Datos
                 valor_default = default_value;
                 llave = key;
                 coment_column = column_comment;
+                this.value = value;
             }
             public string nombre;
             public TipoDeCampo column_type;
@@ -31,14 +32,32 @@ namespace LibBase_Datos
             public object default_value;
             public Indice key;
             public string column_comment;
+            public object value;
         }
     }
 
     public class Usuarios : Tabla
     {
+        public Usuarios()
+        {
+            this.comment = "En esta tabla se guardará la información de los usuarios";
+            this.columns.Add(new Columna("Nombre", TipoDeCampo.Varchar, 50, false, null, Indice.No, "Nombre de los usuarios", ""));
+            this.columns.Add(new Columna("RFC", TipoDeCampo.Varchar, 13, true, null, Indice.No, "RFC para facturación", ""));
+        }
+
         public void CrearCampo(string nom, TipoDeCampo tipo_columna, int largo_campo, bool es_null, object valor_default, Indice llave, string coment_column)
         {
+            this.columns.Add(new Columna(nom, tipo_columna, largo_campo, es_null, valor_default, llave, coment_column));
+        }
+    }
 
+    public class Ventas : Tabla
+    {
+        public Usuarios users = new Usuarios();
+
+        public void DatosUsuarios()
+        {
+            users.columns[0].value = "Alexis";
         }
     }
 
